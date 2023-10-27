1807abb9b97a1bcdd763d2883e6b4dab83bfa995
Amidu99 <amidushamika@gmail.com>
2023-10-27 17:22:50 +0530

    finalize the CustomerController

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -128,3 +128,23 @@ $("#customer_tbl_body, #customer_search_tbl_body").on("click", "tr", function() 
     $("#customer_salary").val(salary);
 });
 
+// search customers
+$('#customer_search_box').on('input', () => {
+    let search_term = $('#customer_search_box').val();
+    if(search_term){
+        $('#customer_search_tbl_body').empty();
+        let results = customer_db.filter((customer) =>
+            customer.customer_id.toLowerCase().startsWith(search_term.toLowerCase()) ||
+            customer.name.toLowerCase().startsWith(search_term.toLowerCase()) ||
+            customer.address.startsWith(search_term.toLowerCase()));
+        results.map((customer, index) => {
+            let record = `<tr><td class="customer_id">${customer.customer_id}</td><td class="name">${customer.name}</td>
+                      <td class="address">${customer.address}</td><td class="salary">${customer.salary}</td></tr>`;
+            $("#customer_search_tbl_body").append(record);
+        });
+    }else{
+        $('#customer_search_tbl_body').empty();
+    }
+});
+
+// v1 finalize
