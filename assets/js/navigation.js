fb661f0a10b99bd3808a9b1d8467c44b2dd5d3b9
Amidu99 <amidushamika@gmail.com>
2023-10-27 16:32:35 +0530

    finalize the navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -2,51 +2,44 @@ $('#customer_main_section').css('display', 'none');
 $('#item_main_section').css('display', 'none');
 $('#order_main_section').css('display', 'none');
 
-$('#brand').on('click', () => {
+$("#brand, #home_nav").on('click', () => {
     $('#home_section').css('display', 'block');
     $('#customer_main_section').css('display', 'none');
     $('#item_main_section').css('display', 'none');
     $('#order_main_section').css('display', 'none');
 });
-$('#home_nav').on('click', () => {
-    $('#home_section').css('display', 'block');
-    $('#customer_main_section').css('display', 'none');
-    $('#item_main_section').css('display', 'none');
-    $('#order_main_section').css('display', 'none');
-});
-$('#customer_nav').on('click', () => {
+$("#customer_nav, #customer_nav_btn").on('click', () => {
     $('#home_section').css('display', 'none');
     $('#customer_main_section').css('display', 'block');
     $('#item_main_section').css('display', 'none');
     $('#order_main_section').css('display', 'none');
 });
-$('#item_nav').on('click', () => {
+$("#item_nav, #item_nav_btn").on('click', () => {
     $('#home_section').css('display', 'none');
     $('#customer_main_section').css('display', 'none');
     $('#item_main_section').css('display', 'block');
     $('#order_main_section').css('display', 'none');
 });
-$('#order_nav').on('click', () => {
+$("#order_nav, #order_nav_btn").on('click', () => {
     $('#home_section').css('display', 'none');
     $('#customer_main_section').css('display', 'none');
     $('#item_main_section').css('display', 'none');
     $('#order_main_section').css('display', 'block');
 });
-$('#customer_nav_btn').on('click', () => {
-    $('#home_section').css('display', 'none');
-    $('#customer_main_section').css('display', 'block');
-    $('#item_main_section').css('display', 'none');
-    $('#order_main_section').css('display', 'none');
-});
-$('#item_nav_btn').on('click', () => {
-    $('#home_section').css('display', 'none');
-    $('#customer_main_section').css('display', 'none');
-    $('#item_main_section').css('display', 'block');
-    $('#order_main_section').css('display', 'none');
-});
-$('#order_nav_btn').on('click', () => {
-    $('#home_section').css('display', 'none');
-    $('#customer_main_section').css('display', 'none');
-    $('#item_main_section').css('display', 'none');
-    $('#order_main_section').css('display', 'block');
-});
+const imageSources = [
+    './assets/images/welcome-img1.png',
+    './assets/images/welcome-img2.png',
+    './assets/images/welcome-img3.png',
+    './assets/images/welcome-img4.png',
+    './assets/images/welcome-img5.png'
+];
+let currentImageIndex = 0;
+const rotatingImage = $('#homeImg');
+function rotateImage() {
+    rotatingImage.fadeOut(500, function() {
+        currentImageIndex = (currentImageIndex + 1) % imageSources.length;
+        rotatingImage.attr('src', imageSources[currentImageIndex]).fadeIn(500);
+    });
+}
+setInterval(rotateImage, 2500);
+AOS.init();
